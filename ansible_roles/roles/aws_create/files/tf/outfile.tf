126f9ff5c21f0ab6bbf7bd78bcc63bd94f8198c7
David Valin <dvalin@redhat.com>
2023-03-23 07:22:07 -0400

    GP3 updates for aws

--- ansible_roles/roles/aws_create/files/tf/outfile.tf
+++ ansible_roles/roles/aws_create/files/tf/outfile.tf
@@ -1,11 +1,11 @@
 # Create info, no spot and no network.
 output "public_dns_host0" {
- value = REPLACE.ec2[0].public_dns
+ value = REPLACE_INST.ec2[0].public_dns
 }
 output "aws_instance_id0" {
- value = REPLACE.ec2[0].REPLACE_ID
+ value = REPLACE_INST.ec2[0].REPLACE_ID
 }
 output "aws_zoneid0" {
- value = REPLACE.ec2[0].availability_zone
+ value = REPLACE_INST.ec2[0].availability_zone
 }
 
