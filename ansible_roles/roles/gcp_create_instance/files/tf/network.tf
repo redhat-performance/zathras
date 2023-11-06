d7c5285ef3aa870ed32665cff79dadf488df0d7d
sousinha97 <sousinha@redhat.com>
2023-11-06 21:40:34 +0530

    add tags to gcp

--- ansible_roles/roles/gcp_create_instance/files/tf/network.tf
+++ ansible_roles/roles/gcp_create_instance/files/tf/network.tf
@@ -3,6 +3,10 @@ resource "google_compute_network" "test-network" {
   count                   = var.network_count
   name                    = "${var.run_label}-${var.project_id}-${var.machine_type}-${count.index}"
   auto_create_subnetworks = false
+  labels = {
+    name = var.run_label
+    project = var.project_id
+  }
 }
 
 resource "google_compute_subnetwork" "test-subnet" {
@@ -10,14 +14,22 @@ resource "google_compute_subnetwork" "test-subnet" {
   name          = "${var.run_label}-${var.project_id}-${var.machine_type}-${count.index}"
   ip_cidr_range = "10.20.3${count.index}.0/24"
   network       = google_compute_network.test-network[count.index].id
+  labels = {
+    name = var.run_label
+    project = var.project_id
+  }
 }
 
 resource "google_compute_firewall" "uperf-ingress" {
   count   = var.network_count
   name    = "${var.run_label}-${var.project_id}-${var.machine_type}-${count.index}"
   network = google_compute_network.test-network[count.index].id
+  labels = {
+    name = var.run_label
+    project = var.project_id
+  }
 
-  # Allow all communication 0.0.0.0/0 on the specific network  
+  # Allow all communication 0.0.0.0/0 on the specific network
   allow {
     protocol = "all"
   }
