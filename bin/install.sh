cd57fe083a3dcda06dd72e278b583798e9143972
Greg Dumas <grdumas@users.noreply.github.com>
2024-04-10 11:42:49 -0400

    Update package installation
    Modify the installation process to check if packages are already installed first. Also adds a double quotation mark to correct a syntax error at the end.

--- bin/install.sh
+++ bin/install.sh
@@ -35,46 +35,54 @@ if (( $EUID != 0 )); then
     echo "Not running as root, proceed."
 fi
 
-# Add the terraform repository from HashiCorp
-# for reference: https://developer.hashicorp.com/terraform/cli/install/yum
-# Currently supported distros: fedora, RHEL
+# check for and install system packages
+packages=(ansible git jq python python3-pip terraform wget)
 
-# Get the operating system distribution
-os_release=$(grep "^ID=" /etc/os-release | awk -F'=' '{print $2}')
+for package in "${packages[@]}"; do 
+    if dnf list installed "$package" &> /dev/null; then
+        echo "$package is installed."
+    elif dnf list "$package" &> /dev/null; then
+        echo "$package is not installed but available. Installing..."
+        sudo dnf install -y "$package"
+    elif [ $package == "terraform" ]; then
+        # Add the terraform repository from HashiCorp
+        # currently supported distros: fedora, RHEL
+        # reference: https://developer.hashicorp.com/terraform/cli/install/yum
 
-# Sometimes $release contains quotes that need to be removed
-os_release_clean=$(echo $os_release | tr -d '"')
+        # Get operating system distribution
+        os_release=$(grep "^ID=" /etc/os-release | awk -F'=' '{print $2}')
 
-# The HashiCorp repository urls are case-sensitive
-if [ $os_release_clean = 'rhel' ]; then
-    release='RHEL'
-fi
+        # Sometimes the $release contains quotes that need to be removed
+        os_release_clean=$(echo $os_release | tr -d '"')
 
-# fedora
-if [ $os_release_clean = 'fedora' ]; then
-    release='fedora'
-fi
+        # HashiCorp repo urls are case-sensitive
+        if [ $os_release_clean = 'rhel' ]; then
+            release='RHEL'
+        elif [ $os_release_clean = 'fedora' ]; then
+            release='fedora'
+        fi
 
-# repo URL for terraform
-repo_url="https://rpm.releases.hashicorp.com/${release}/hashicorp.repo"
+        # repo URL for terraform
+        repo_url="https://rpm.releases.hashicorp.com/${release}/hashicorp.repo"
 
-# run dnf config-manager
-dnf config-manager --add-repo $repo_url
+        # run dnf config-manager
+        sudo dnf config-manager --add-repo $repo_url
 
+        # install the package
+        sudo dnf install terraform -y
+    else
+        echo "package is not installed and not available."
+    fi
 
-# install other required packages
-dnf update -y
-dnf install ansible git jq python python3-pip terraform wget -y
+done
 
 
 # pip install requirements
 pip3 install -U boto --user
 pip3 install yq --user
 
-# add yq install to path
-export PATH=/usr/local/bin:$PATH
 
-echo "Before you can run Zathras:
+echo "Before you can run Zathras:"
 echo "****Set up a scenario file"
 echo "****If running a local system, create the local config file for the system under test (SUT)"
 echo "****If using an existing system, ssh-copy-id between the system Zathras is installed on and the SUT."
