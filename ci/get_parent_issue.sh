320d43b4f9bed498c2743fafee8ec70aef2321ff
Keith Valin <kvalin@redhat.com>
2025-01-17 14:40:16 -0500

    Fix: Add ability to manage multiple issues at the same time

--- ci/get_parent_issue.sh
+++ ci/get_parent_issue.sh
@@ -4,6 +4,6 @@ mentioned_issues=$(gh pr view "$1" --json body | jq -r .body | grep -Eo '#[0-9]+
 
 for issue in $mentioned_issues; do
 	if gh issue view "$issue" --json id > /dev/null; then
-		echo "$issue"
+		echo -n  "$issue "
 	fi
 done
