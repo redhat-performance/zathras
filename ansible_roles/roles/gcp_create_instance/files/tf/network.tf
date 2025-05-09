f14ddc0704edfb4fdebfc230327190112db3be62
Greg Dumas <gdumas@redhat.com>
2025-05-09 10:19:27 -0400

    Remove GCP network labels

--- ansible_roles/roles/gcp_create_instance/files/tf/network.tf
+++ ansible_roles/roles/gcp_create_instance/files/tf/network.tf
@@ -3,11 +3,6 @@ resource "google_compute_network" "test-network" {
   count                   = var.network_count
   name                    = "${var.run_label}-${var.project_id}-${var.machine_type}-${count.index}"
   auto_create_subnetworks = false
-  labels = {
-    name = var.run_label
-    user = var.User
-    project = var.Project
-  }
 }
 
 resource "google_compute_subnetwork" "test-subnet" {
@@ -15,21 +10,12 @@ resource "google_compute_subnetwork" "test-subnet" {
   name          = "${var.run_label}-${var.project_id}-${var.machine_type}-${count.index}"
   ip_cidr_range = "10.20.3${count.index}.0/24"
   network       = google_compute_network.test-network[count.index].id
-  labels = {
-    name = var.run_label
-    user = var.User
-    project = var.Project
-  }
 }
 
 resource "google_compute_firewall" "uperf-ingress" {
   count   = var.network_count
   name    = "${var.run_label}-${var.project_id}-${var.machine_type}-${count.index}"
   network = google_compute_network.test-network[count.index].id
-  labels = {
-    name = var.run_label
-    user = var.User
-    project = var.Project
   }
 
   # Allow all communication 0.0.0.0/0 on the specific network
