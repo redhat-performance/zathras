3eb6da09fff76ea1b7829f520bf8660935af8bfa
David Valin <dvalin@redhat.com>
2026-03-06 06:28:36 -0500

    ansible_vars_main.yml is still used by determine_test_status, that will go away when we add the new test validation

--- bin/kick_off.sh
+++ bin/kick_off.sh
@@ -305,5 +305,5 @@ rm -f *status
 #
 # Remove  misc files
 #
-rm -rf terraform_data.yml test_info upload* ignore.yml tags_defaults ansible.cfg config ansible_install_group add_vars_tf add_main_vars.tf add_main_tf_vars ansible_test_group ansible_vars_main.yml
+rm -rf terraform_data.yml test_info upload* ignore.yml tags_defaults ansible.cfg config ansible_install_group add_vars_tf add_main_vars.tf add_main_tf_vars ansible_test_group
 exit 0
